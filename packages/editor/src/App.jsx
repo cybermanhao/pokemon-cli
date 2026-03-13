5efa00b08e4b9cfca9b0a8070da239bca59583cb
Developer <dev@pokemon.local>
2026-03-13 13:07:47 -0300

    feat(editor): add tauri editor app base

---
+++ packages/editor/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Pokemon Editor</h1>
+      <p>Editor modules will go here:</p>
+      <ul>
+        <li>Sprite Editor</li>
+        <li>Map Editor</li>
+        <li>Pokedex Editor</li>
+        <li>Move Editor</li>
+        <li>Item Editor</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
